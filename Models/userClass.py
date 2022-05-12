61a31bb9321d680157994463a8b05cbfb83234fd
dilpreet <dilpreetreehal@gmail.com>
2022-05-12 21:50:02 +0100

    Qmessage box and closes window

--- Models/userClass.py
+++ Models/userClass.py
@@ -3,19 +3,13 @@ class User():
     def __init__(self, name, password):
         self.username = name
         self.password = password
-        self.loggedIn=False
+        self.loggedIn = False
 
 
     def login(self):
         db = Database()
         self.loggedIn=db.loginQuery(self.username,self.password)
-
-
-
-
-
-
-
+        return self.loggedIn
 
 
     def takePicture(self):
